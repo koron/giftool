2231b1477742a1e3e40d612ef58bb091ebcce7a9
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-12-18 18:12:46 +0900

    extract representative image

---
+++ internal/extract/common.go
@@ -0,0 +1,160 @@
+package extract
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"math"
+	"os"
+)
+
+func prepareExpose(outdir, input string) (*gif.GIF, error) {
+	f, err := os.Open(input)
+	if err != nil {
+		return nil, err
+	}
+	g, err := gif.DecodeAll(f)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	err = os.MkdirAll(outdir, 0777)
+	if err != nil {
+		return nil, err
+	}
+	return g, nil
+}
+
+func forComposedFrames(g *gif.GIF, fn func(int, *image.RGBA) error) error {
+	buf := image.NewRGBA(image.Rect(0, 0, g.Config.Width, g.Config.Height))
+	for i, img := range g.Image {
+		err := composeImages(buf, img, g.Disposal[i], g.BackgroundIndex)
+		if err != nil {
+			return err
+		}
+		err = fn(i, buf)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func averagingImages(images []*image.RGBA, delay []int) *image.RGBA {
+	first := images[0]
+	width, height := first.Rect.Dx(), first.Rect.Dy()
+
+	buf := make([]uint64, width*height*4)
+	var wsum uint64
+	for i, img := range images {
+		weight := uint64(delay[i])
+		if weight == 0 {
+			weight = 1
+		}
+		wsum += weight
+		for y := range height {
+			for x := range width {
+				c := img.RGBAAt(x, y)
+				j := (x + y*width) * 4
+				buf[j+0] += uint64(c.R) * weight
+				buf[j+1] += uint64(c.G) * weight
+				buf[j+2] += uint64(c.B) * weight
+				buf[j+3] += uint64(c.A) * weight
+			}
+		}
+	}
+
+	avg := image.NewRGBA(image.Rect(0, 0, width, height))
+	for y := range height {
+		for x := range width {
+			idx := (x + y*width) * 4
+			c := color.RGBA{
+				R: uint8(buf[idx+0] / wsum),
+				G: uint8(buf[idx+1] / wsum),
+				B: uint8(buf[idx+2] / wsum),
+				A: uint8(buf[idx+3] / wsum),
+			}
+			avg.Set(x, y, c)
+		}
+	}
+
+	return avg
+}
+
+type Lab struct {
+	L float64
+	A float64
+	B float64
+}
+
+func gammaCorrection(v float64) float64 {
+	if v <= 0.04045 {
+		return v / 12.92
+	}
+	return math.Pow((v+0.055)/1.055, 2.4)
+}
+
+func labHelper(v float64) float64 {
+	if v > 0.008856 {
+		return math.Pow(v, 1.0/3.0)
+	}
+	return (903.3*v + 16) / 116
+}
+
+func rgbaToLab(c color.RGBA) Lab {
+	r := float64(c.R) / 255.0
+	g := float64(c.G) / 255.0
+	b := float64(c.B) / 255.0
+
+	r = gammaCorrection(r)
+	g = gammaCorrection(g)
+	b = gammaCorrection(b)
+
+	x := r*0.4124 + g*0.3576 + b*0.1805
+	y := r*0.2126 + g*0.7152 + b*0.0722
+	z := r*0.0193 + g*0.1192 + b*0.9505
+
+	// XYZ -> Lab
+	x = labHelper(x / 0.95047)
+	y = labHelper(y / 1.00000)
+	z = labHelper(z / 1.08883)
+
+	return Lab{
+		L: math.Max(0, 116*y-16),
+		A: 500 * (x - y),
+		B: 200 * (y - z),
+	}
+}
+
+func diffColors(a, b color.RGBA) color.Gray16 {
+	la := rgbaToLab(a)
+	lb := rgbaToLab(b)
+	d := math.Sqrt(math.Pow(la.L-lb.L, 2) + math.Pow(la.A-lb.A, 2) + math.Pow(la.B-lb.B, 2))
+	return color.Gray16{Y: uint16(d * 0xffff)}
+}
+
+func diffImages(a, b *image.RGBA) *image.Gray16 {
+	width, height := a.Rect.Dx(), a.Rect.Dy()
+	diff := image.NewGray16(image.Rect(0, 0, width, height))
+	for y := range height {
+		for x := range width {
+			ca := a.RGBAAt(x, y)
+			cb := b.RGBAAt(x, y)
+			d16 := diffColors(ca, cb)
+			diff.SetGray16(x, y, d16)
+		}
+	}
+	return diff
+}
+
+func measureGray16(g *image.Gray16) float64 {
+	width, height := g.Rect.Dx(), g.Rect.Dy()
+	var sum float64
+	for y := range height {
+		for x := range width {
+			c := g.Gray16At(x, y)
+			sum += float64(c.Y) / float64(0xffff)
+		}
+	}
+	return sum / (float64(width) * float64(height))
+}
--- internal/extract/extract.go
+++ internal/extract/extract.go
@@ -7,60 +7,113 @@ import (
 	"fmt"
 	"image"
 	"image/draw"
-	"image/gif"
 	"image/png"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/koron-go/subcmd"
+)
+
+type exposeTarget int
+
+const (
+	targetFrames exposeTarget = iota
+	targetComposed
+	targetRepresentative
 )
 
 var Extract = subcmd.DefineCommand("extract", "extract each frames from GIF", func(ctx context.Context, args []string) error {
 	fs := subcmd.FlagSet(ctx)
 	var (
-		outdir  string
-		compose bool
+		outdir string
+		target string
 	)
 	fs.StringVar(&outdir, "outdir", "", "output directory (default: base of input)")
-	fs.BoolVar(&compose, "compose", false, "compose images")
+	fs.StringVar(&target, "target", "frames", "what to extract")
 	fs.Parse(args)
+
+	var exposeMode exposeTarget
+	switch strings.ToLower(target) {
+	case "frames":
+		exposeMode = targetFrames
+	case "composed":
+		exposeMode = targetComposed
+	case "representative":
+		exposeMode = targetRepresentative
+	default:
+		return fmt.Errorf("unknown expose target: %q", target)
+	}
+
 	if fs.NArg() != 1 {
 		return errors.New("only one GIF can be extracted at a time")
 	}
 	input := fs.Arg(0)
+
 	if outdir == "" {
 		outdir = input[0 : len(input)-len(filepath.Ext(input))]
 	}
-	if compose {
+
+	switch exposeMode {
+	case targetFrames:
+		return extractFrames(outdir, input)
+	case targetComposed:
 		return extractComposedFrames(outdir, input)
+	case targetRepresentative:
+		return extractRepresentative(outdir, input)
+	default:
+		panic("invalid expose mode")
 	}
-	return extractFrames(outdir, input)
 })
 
-func extractComposedFrames(outdir, input string) error {
-	f, err := os.Open(input)
+func extractRepresentative(outdir, input string) error {
+	g, err := prepareExpose(outdir, input)
 	if err != nil {
 		return err
 	}
-	g, err := gif.DecodeAll(f)
-	f.Close()
-	if err != nil {
-		return err
-	}
-	os.MkdirAll(outdir, 0777)
 
-	buf := image.NewRGBA(image.Rect(0, 0, g.Config.Width, g.Config.Height))
-	for i, img := range g.Image {
-		err := composeImages(buf, img, g.Disposal[i], g.BackgroundIndex)
+	images := make([]*image.RGBA, len(g.Image))
+	err = forComposedFrames(g, func(i int, img *image.RGBA) error {
+		copy := image.NewRGBA(image.Rect(0, 0, g.Config.Width, g.Config.Height))
+		draw.Draw(copy, img.Rect, img, img.Rect.Min, draw.Over)
+		images[i] = copy
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	avg := averagingImages(images, g.Delay)
+	output := filepath.Join(outdir, "000avg.png")
+	err = writeImage(output, avg)
+	if err != nil {
+		return err
+	}
+
+	for i, img := range images {
+		g16 := diffImages(avg, img)
+		p := measureGray16(g16)
+		fmt.Printf("#%-3d %f\n", i, p)
+		output := filepath.Join(outdir, fmt.Sprintf("%03dd.png", i))
+		err := writeImage(output, g16)
 		if err != nil {
 			return err
 		}
-		output := filepath.Join(outdir, fmt.Sprintf("%03d.png", i))
-		if err := writeImage(output, buf); err != nil {
-			return err
-		}
 	}
+
 	return nil
+}
+
+func extractComposedFrames(outdir, input string) error {
+	g, err := prepareExpose(outdir, input)
+	if err != nil {
+		return err
+	}
+
+	return forComposedFrames(g, func(i int, img *image.RGBA) error {
+		output := filepath.Join(outdir, fmt.Sprintf("%03dc.png", i))
+		return writeImage(output, img)
+	})
 }
 
 func composeImages(dst *image.RGBA, src *image.Paletted, disposal, bgIndex byte) error {
@@ -74,16 +127,10 @@ func composeImages(dst *image.RGBA, src *image.Paletted, disposal, bgIndex byte)
 }
 
 func extractFrames(outdir, input string) error {
-	f, err := os.Open(input)
+	g, err := prepareExpose(outdir, input)
 	if err != nil {
 		return err
 	}
-	g, err := gif.DecodeAll(f)
-	f.Close()
-	if err != nil {
-		return err
-	}
-	os.MkdirAll(outdir, 0777)
 
 	for i, img := range g.Image {
 		output := filepath.Join(outdir, fmt.Sprintf("%03d.png", i))
