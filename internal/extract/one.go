3a7bdd16af5b7e2d76d5705c1f4fd2aa3512226a
MURAOKA Taro <koron.kaoriya@gmail.com>
2025-01-22 13:35:41 +0900

    add mode to measure entropy of colored image

--- internal/extract/one.go
+++ internal/extract/one.go
@@ -11,8 +11,13 @@ import (
 	"github.com/koron/giftool/internal/imgutil"
 )
 
+// ExtractRepFrame extracts a representative frame from an animation GIF.
+// "representative" means most "informational": highest entropy of image.
 func ExtractRepFrame(ctx context.Context, args []string) error {
+	var coloredentropy bool
+
 	fs := subcmd.FlagSet(ctx)
+	fs.BoolVar(&coloredentropy, "coloredentropy", false, "use colored image to measure entropy (default: gray scaled)")
 	fs.Parse(args)
 
 	if fs.NArg() != 1 {
@@ -21,7 +26,13 @@ func ExtractRepFrame(ctx context.Context, args []string) error {
 	input := fs.Arg(0)
 
 	output := appendFilename(input, "_rep.png")
-	return extractRepresentativeFrame(output, input)
+
+	entropyFunc := normalEntropy
+	if coloredentropy {
+		entropyFunc = colorEntropy
+	}
+
+	return extractRepresentativeFrame(output, input, entropyFunc)
 }
 
 type frameInfo struct {
@@ -30,7 +41,15 @@ type frameInfo struct {
 	entropy float64
 }
 
-func extractRepresentativeFrame(output, input string) error {
+func normalEntropy(img image.Image) float64 {
+	return imgutil.MeasureEntropy(imgutil.ToGray(img))
+}
+
+func colorEntropy(img image.Image) float64 {
+	return imgutil.MeasureEntropy(img)
+}
+
+func extractRepresentativeFrame(output, input string, measureEntropy func(image.Image) float64) error {
 	g, err := gifutil.LoadFile(input)
 	if err != nil {
 		return err
@@ -38,8 +57,7 @@ func extractRepresentativeFrame(output, input string) error {
 
 	highest := frameInfo{i: -1, entropy: -1}
 	for i, img := range gifutil.IterateComposed(g) {
-		gray := imgutil.ToGray(img)
-		entropy := imgutil.MeasureEntropy(gray)
+		entropy := measureEntropy(img)
 		if entropy > highest.entropy {
 			highest = frameInfo{i: i, img: img, entropy: entropy}
 		}
